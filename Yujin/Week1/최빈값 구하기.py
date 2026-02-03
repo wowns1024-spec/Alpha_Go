4780e7afd3afa8f9ef00fdbaa8ce9762d6e2d73c
유진 <eu9309@naver.com>
2026-02-03 09:30:37 +0900

    Week1_최빈값 구하기_Yujin

--- Yujin/Week1/최빈값 구하기.py
+++ Yujin/Week1/최빈값 구하기.py
@@ -1,15 +1,10 @@
 def solution(array):
-    # 개수 카운트용 리스트 생성 (인덱스 = 해당 원소)
     count = [0] * (max(array)+1)
-
-    # 원소 개수 카운트
     for i in array:
         count[i] += 1
     
-    # 리스트 내 최댓값의 인덱스 반환 = 원소 
     answer = count.index(max(count))
 
-    # 같은 개수를 가지는 원소가 또 있는지 검사
     value = max(count)
     count.pop(count.index(max(count)))
     for i in count:
