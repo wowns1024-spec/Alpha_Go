f09201bbb0c3c8d26d55501e2ab341f0039ac4df
wlsalfl321 <wlsalfl321@naver.com>
2026-02-06 02:54:28 +0900

    0128_K번째수_Miri

---
+++ Miri/K번째수.py
@@ -0,0 +1,7 @@
+def solution(arry, commands):
+    result = []
+    for i, j, k in commands:
+        sliced = arry[i-1:j]  # i~j 자르기 # 파이썬은 0부터 시작이니까 i-1
+        sliced.sort()  #정렬
+        result.append(sliced[k-1]) # k번째 수 (k번째 -> k-1)
+    return result
